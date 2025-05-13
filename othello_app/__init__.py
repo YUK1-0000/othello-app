d0a076b52451060b9d4eaadeae3f26245b0ab34f
YUK1 <tanaka543taro@gmail.com>
2025-05-13 21:58:39 +0900

    ライブラリ化

--- othello_app/__init__.py
+++ othello_app/__init__.py
@@ -1,3 +1,4 @@
+from .constants import *
 from .model import Model
 from .view import SuperFrame, MenuBar
 from .controller import Controller
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="othello_app",
+    version="1.1.0",
+    packages=find_packages(),
+)
